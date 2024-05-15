adc82943de160a0fd2f27149da5f78bae6483c7d
pjstyle07 <pankajsjadhav0707@gmail.com>
2024-05-15 18:00:42 +0530

    configuration added

---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+export const environment = {
+  production: true,
+
+  // socialLinks
+
+  fbLink: 'Sorry not found',
+  instaLink: 'https://instagram.com/pjstyle_0707',
+  youtubeLink: 'https://www.youtube.com/channel/UCfjIKeWPK-wGItW1UyTyRDA',
+  linkedinLink: 'https://www.linkedin.com/in/p%C3%A0nkaj-jadhav-560b63220/',
+  githubLink: 'https://github.com/pjstyle07',
+
+
+  // email js
+
+  // create email js acccount
+
+  mailService: "service_m0jghyg",
+  templateId: 'service_m0jghyg',
+  emailJSKey: 'service_m0jghyg',
+
+  // project
+
+  projectOne: 'Enter your Project link',
+  projectTwo: 'Enter your Project link',
+  projectThree:"Enter your Project link",
+
+};
